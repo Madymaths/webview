e4cf8a077141e1d45933d9f3bfef2c1a9b4fda18
Mathankumar R <mathan@constient.com>
2023-12-21 13:35:32 +0530

    first commit

---
+++ app/src/main/java/com/android/webviewandroid/InitialActivity.kt
@@ -0,0 +1,35 @@
+package com.android.webviewandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.android.webviewandroid.databinding.ActivityInitialBinding
+
+
+class InitialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitialBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        redirectScreen()
+    }
+
+    private fun redirectScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }, 2000)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
